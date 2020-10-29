a4401f3ed909c4977af0932a142ff93189204715
ulissesms <ulisses.sousa@edu.unipar.br>
2020-10-29 08:44:03 -0300

    adicionado dados  para teste

--- MODULO02/backend/data.js
+++ MODULO02/backend/data.js
@@ -4,5 +4,41 @@ module.exports[
     title: 'Red nose, black nose e Blue nose.',
     description: 'Falando sobre PitMoster.',
     durations:'50min',
+  },
+  {
+    id: 'dln7188Snc',
+    title: 'Red nose, black nose e Blue nose.',
+    description: 'Falando sobre PitMoster.',
+    durations:'50min',
+  },
+  {
+    id: 'dln7188Snc',
+    title: 'Red nose, black nose e Blue nose.',
+    description: 'Falando sobre PitMoster.',
+    durations:'50min',
+  },
+  {
+    id: 'dln7188Snc',
+    title: 'Red nose, black nose e Blue nose.',
+    description: 'Falando sobre PitMoster.',
+    durations:'50min',
+  },
+  {
+    id: 'dln7188Snc',
+    title: 'Red nose, black nose e Blue nose.',
+    description: 'Falando sobre PitMoster.',
+    durations:'50min',
+  },
+  {
+    id: 'dln7188Snc',
+    title: 'Red nose, black nose e Blue nose.',
+    description: 'Falando sobre PitMoster.',
+    durations:'50min',
+  },
+  {
+    id: 'dln7188Snc',
+    title: 'Red nose, black nose e Blue nose.',
+    description: 'Falando sobre PitMoster.',
+    durations:'50min',
   }
 ]
